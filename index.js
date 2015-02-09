3aa810178ba0bc088a7247c8abbc530d624a9876
Peter de Croos <cultofmetatron@aumlogic.com>
2015-02-09 13:02:44 -0800

    created initial files

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/*
+ * Developer Test Question
+# A bunch of kids are about to roll down a hill and they are deciding what order to roll in.
+# Write a function that takes an array of names and modifies the array such that "jack"
+# is always first, and "jill" is always second (because she came tumbling after).
+ 
+# Show your (hopefully robust) test cases and state your assumptions
+# Use any language - but bonus points for using coffee-script
+ 
+# example 1: input = ['bob', 'jill', 'jack', 'amanda'], output ['jack', 'jill', 'bob', 'amanda']
+# example 2: input = ['bob', 'jill', 'amanda'], output ['jill', 'bob', 'amanda']
+# example 3: input = ['bob', 'jack', 'jill', 'jack', 'amanda'], output ['jack', 'jack', 'jill', 'bob', 'amanda']
+
+*/
+
+
+var jacknjill = function(array) {
+  //make a copy of the array so that we aren't messing with the original
+  var workspace = array.slice(); 
+}
+
+
+
